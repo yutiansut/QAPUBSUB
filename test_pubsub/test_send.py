f795c39f3f15d2d0d34016339ce6922a13615d09
yutiansut <yutiansut@qq.com>
2020-03-17 15:02:15 +0800

    #

--- test_pubsub/test_send.py
+++ test_pubsub/test_send.py
@@ -4,4 +4,4 @@ import json
 
 c = producer.publisher_routing(exchange='qamdgateway', host='192.168.2.117', durable=True)
 
-c.pub(json.dumps({'account_cookie': 'aa', 'code': 'SZ000001'}), routing_key='All')
+c.pub(json.dumps({'account_cookie': 'aa', 'code': '000001'}), routing_key='All')
